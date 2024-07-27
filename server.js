5ca9691e96a30ef761de45981b1a0d074a4b2b8f
Ayelen <ayelen.celis92@gmail.com>
2024-07-27 16:00:04 -0500

    3

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import express from 'express';
-const path = require('path');
-const app = express();
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
